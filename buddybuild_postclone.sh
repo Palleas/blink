155f2a2123f9ac4867844e8b0b9a3ad92ca39184
Romain Pouclet <romain.pouclet@gmail.com>
2017-07-18 10:54:56 -0400

    Tweak config file before building

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 ./get_frameworks.sh
+
+# Tweak the configuration file form UICKeyChainStore
+
+CONFIG_PATH="Frameworks/UICKeyChainStore/Lib/Configurations/UICKeyChainStore.xcconfig"
+
+echo "" >> $CONFIG_PATH
+echo "// In case you're reading this, what the hell are you doing here?" >> $CONFIG_PATH
+echo "VALID_ARCHS = i386 x86_64 armv7 arm64 armv7s" >> $CONFIG_PATH
